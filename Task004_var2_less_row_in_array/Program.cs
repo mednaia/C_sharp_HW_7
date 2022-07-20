bb0b80249af56f9774a8e04c5c427b02eaa13d88
Ekaterina <tiurina.kate@gmail.com>
2022-07-20 13:58:02 +0300

    variant with sum of the first row

---
+++ Task004_var2_less_row_in_array/Program.cs
@@ -0,0 +1,60 @@
+﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов
+
+int[,] CreateMatrix(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] matrix = new int[rows,columns];
+    var random = new Random();
+    for(int i=0;i<rows;i++)
+        for(int j=0;j<columns;j++)
+            matrix[i,j] = random.Next(minValue,maxValue);
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i=0; i<matrix.GetLength(0); i++)
+    {
+        for(int j=0; j<matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i,j]} ");
+    Console.WriteLine();
+    }
+}
+
+void SearchLessRow(int[,] matrix)
+{
+    int minI = 0;
+    int minSum = 0;
+    int tmpSum1 = 0;
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        tmpSum1 += matrix[0, j];
+    }
+    minSum = tmpSum1;
+    for(int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        int tmpSum2 = 0;
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            tmpSum2 += matrix[i, j];
+        if(tmpSum2 < tmpSum1)
+        {   
+            minSum = tmpSum2; 
+            minI = i;
+        }
+        tmpSum1 = tmpSum2;
+    }
+    Console.WriteLine($"The least value in row with index {minI} with sum {minSum}.");
+}
+
+Console.Write("Enter amount of matrix rows: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter amount of matrix columns: ");
+int columns = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter left side of matrix columns: ");
+int start = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter right side of matrix columns: ");
+int finish = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = CreateMatrix(rows,columns,start,finish);
+Console.WriteLine("Your matrix: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+SearchLessRow(matrix);
